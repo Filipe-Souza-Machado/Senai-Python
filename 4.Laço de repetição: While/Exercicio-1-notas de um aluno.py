970c4f3e5b4c4f80c7c760b3d8ae464e20cd0adb
Filipe-Souza-Machado <159461643+Filipe-Souza-Machado@users.noreply.github.com>
2024-04-16 17:27:59 +0000

    piton

---
+++ 4.Laço de repetição: While/Exercicio-1-notas de um aluno.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("cls || clear");
+
+nota: float = -1;
+
+while nota < 0 or nota > 10:
+    nota = float(input("Digite sua nota: "));
+    os.system("cls || clear");
+
+print(f"Nota: {nota}");
