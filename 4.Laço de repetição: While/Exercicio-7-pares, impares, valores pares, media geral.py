83712636a6b947f9891b3bb55455156ebc41d510
Filipe-Souza-Machado <159461643+Filipe-Souza-Machado@users.noreply.github.com>
2024-04-18 19:49:43 +0000

    pintao

---
+++ 4.Laço de repetição: While/Exercicio-7-pares, impares, valores pares, media geral.py
@@ -0,0 +1,25 @@
+import os
+
+os.system("cls || clear");
+
+numero: int;
+contador: int = 0;
+pares: int = 0;
+impares: int = 0;
+soma: int = 0;
+mediaPar: int;
+mediaGeral: int;
+
+while numero != 0 and numero > 0:
+
+    numero = int(input("Digite um numero: "));
+    
+    contador += 1;
+
+    if numero %2 ==0:
+        pares +=1;
+    
+    else:
+        impares +=1;
+
+mediaPar = 
---
+++ 4.Laço de repetição: While/Exercicio-8-Aivivdade no papel.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("cls || clear");
+
+idade:int;
+sexo:str;
+salario:float;
+mediaSalarial:float;
+soma:int = 0;
+contador:int = 0;
+contadorFem:int = 0;
+menu:int;
