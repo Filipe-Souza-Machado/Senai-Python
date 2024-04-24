daf447b1fa0cde66419aeadce883b4de3652ba73
Filipe-Souza-Machado <159461643+Filipe-Souza-Machado@users.noreply.github.com>
2024-04-24 23:46:41 +0000

    python

--- Atividades independentes/UFU-7.py
+++ Atividades independentes/UFU-7.py
@@ -1,3 +1,5 @@
+#Escreva um programa que leia 10 numeros inteiros e os armazene em um vetor. Imprima ´
+#o vetor, o maior elemento e a posic¸ao que ele se encontra.
 import os;
 
 os.system("cls || clear");
