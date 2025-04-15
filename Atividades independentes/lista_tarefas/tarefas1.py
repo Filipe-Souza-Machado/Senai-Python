1669dbc9af3f351821370ecee4dc1299fce62003
Filipe-Souza-Machado <159461643+Filipe-Souza-Machado@users.noreply.github.com>
2025-04-15 16:27:13 -0300

    Add files via upload

---
+++ Atividades independentes/lista_tarefas/tarefas1.py
@@ -0,0 +1,100 @@
+#IMPORTS---------------------------------------------------------------------
+import customtkinter as ctk
+from tkinter import *
+from tkinter import messagebox
+
+#FUNÇÕES---------------------------------------------------------------------
+def add_tarefa():
+    t = tarefa.get();
+    
+    if (t):
+        lista_tarefas.insert(0,t);
+        tarefa.delete(0,'end');
+        salvar_tarefas();
+    else:
+        messagebox.showerror('Error ' ,' O campo tarefa esta vazio!!');
+
+def del_tarefa():
+    
+    selecionada = lista_tarefas.curselection();
+    if (selecionada):
+        lista_tarefas.delete(selecionada);
+        salvar_tarefas();
+    else:
+        messagebox.showerror('Error ' ,' Nenhuma tarefa foi selecionada!!'); 
+
+def salvar_tarefas():
+    with open('tarefas.txt','w') as ta:
+        tarefas = lista_tarefas.get(0, END);
+        for x in tarefas:
+            ta.write(x+'\n');
+
+def carregar_tarefas():
+    with open('tarefas.txt', 'r') as ta:
+        tarefas = ta.readlines()
+        
+        for x in tarefas:
+            lista_tarefas.insert(0 , x.strip());
+#----------------------------------------------------------------------------
+
+#Janela----------------------------------------------------------------------
+janela = ctk.CTk();
+ctk.set_appearance_mode("dark");
+janela.title("Lista de tarefas - 1.0");
+janela.geometry("350x500");
+janela.resizable(False,False);
+#----------------------------------------------------------------------------
+
+#INPUT-----------------------------------------------------------------------
+tarefa = ctk.CTkEntry(janela,
+                      width=320,
+                      height=40,
+                      border_color='hotpink',
+                      placeholder_text='Digite a tarefa a ser criada',
+                      );
+tarefa.pack(pady=10);
+#----------------------------------------------------------------------------
+
+#BUTõES----------------------------------------------------------------------
+btnAdicionar = ctk.CTkButton(janela,
+                             width=100,
+                             height=40,
+                             fg_color='lightgreen',
+                             hover_color='darkgreen',
+                             text='➕Adicionar tarefa',
+                             font=('verdana',15,'bold'),
+                             cursor='hand2',
+                             text_color='black',
+                             command=add_tarefa
+                             );
+btnAdicionar.place(x=20,y=80);
+
+btnDeletar = ctk.CTkButton(janela,
+                             width=100,
+                             height=40,
+                             fg_color='red',
+                             hover_color='darkred',
+                             text='Deletar tarefa',
+                             font=('verdana',15,'bold'),
+                             cursor='hand2',
+                             text_color='white',
+                             command=del_tarefa
+                             );
+btnDeletar.place(x=205,y=80);
+#------------------------------------------------------------------------------
+
+#LISTA-------------------------------------------------------------------------
+lista_tarefas = Listbox(janela,
+                        width=28,
+                        height=17,
+                        font=('verdana',12,'bold'),
+                        fg='white',
+                        background='#323232',
+                        highlightbackground='hotpink',
+                        highlightthickness=2,                        
+                        );
+lista_tarefas.place(x=21, y=150);
+#-------------------------------------------------------------------------------
+
+carregar_tarefas();
+janela.mainloop();
